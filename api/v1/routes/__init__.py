66797ca0f956712d0454d2b6db7182d659e8af00
Lexmanthefirst <lexmanthefirst@gmail.com>
2025-11-17 13:58:42 +0100

    feat: Integrate authentication routes into API

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .waitlist import router as waitlist_router
+from .auth.auth import router as auth_router
 
 app = APIRouter()
 app.include_router(waitlist_router)
+app.include_router(auth_router)
