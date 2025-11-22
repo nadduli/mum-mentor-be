9783a4c43da3e406f4a4645adf03e00404583258
Ibukun David Babatunde <david.babatunde@um6p.ma>
2025-11-22 11:34:16 +0100

    Fix: added defaultdict import for validation handler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from api.utils.responses import validation_error_response
 from api.v1.routes import app as api_v1_router
+from collections import defaultdict
 
 import logging
 
