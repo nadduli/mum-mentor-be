7230a5eba9952cec1f0acb417baaf2660f876ffc
iMuaz <imuazyakub@gmail.com>
2025-11-24 20:07:44 +0100

    feat: implement chat session and message handling routes and services

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -24,6 +24,7 @@ from .task.edit_task import router as edit_task_router
 from .task.toggle_completion import router as toggle_completion_router
 from .task.delete_task import router as delete_task_router
 from .logout import router as logout_router
+from .chats import chat_router
 
 app = APIRouter()
 
@@ -47,6 +48,9 @@ app.include_router(task_list_router)
 app.include_router(edit_task_router)
 app.include_router(toggle_completion_router)
 app.include_router(delete_task_router)
+
+# Chat routes
+app.include_router(chat_router)
 
 
 # Other routes
