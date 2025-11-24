6e34c2e008309f4337000e96832f596ac8a77b0b
Obianuju Ochuba <ochubaju@gmail.com>
2025-11-24 14:06:23 +0000

    define logic to conform fastapi and pydantic errors to project format

---
+++ api/utils/exception_handlers.py
@@ -0,0 +1,28 @@
+from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
+
+from api.utils.responses import fail_response
+
+
+def request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    """
+    Override default Pydantic/FastAPI validation errors and make it conform to project-defined fail_response format.
+    """
+    return fail_response(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        message="Invalid request data",
+        context={"errors": exc.errors()}
+    )
+
+def http_exception_handler(request: Request, exc: HTTPException):
+    """
+    Override default HTTPException responses (e.g., raise HTTPException(...)).
+    """
+    return fail_response(
+        status_code=exc.status_code,
+        message=exc.detail,
+        context={}
+    )
