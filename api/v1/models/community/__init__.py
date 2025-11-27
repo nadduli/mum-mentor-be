452274571c850b7cef1ca7b36aa633fbc13efa8f
idyweb <inyangidongesit22@gmail.com>
2025-11-27 17:11:58 +0100

    fix: resolve milestone model conflict and simplify schema

--- api/v1/models/community/__init__.py
+++
@@ -1,8 +0,0 @@
-from .posts import Post
-from .post_photos import PostPhoto
-from .post_likes import PostLikes
-from .post_comments import PostComments
-
-
-
-__all__ = ["Post", "PostPhoto", "PostLikes", "PostComments"]
