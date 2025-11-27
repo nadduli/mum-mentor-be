8ffe705282da62d3429db7f0ea75a6f00a7eb7b4
Iwegbu Jedidiah <76275625+whotterre@users.noreply.github.com>
2025-11-27 15:03:09 +0100

    Add post_id column to post_comments model

--- api/v1/models/community/post_comments.py
+++ api/v1/models/community/post_comments.py
@@ -7,5 +7,6 @@ class PostComments(BaseModel):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), nullable=False)
+    post_id = Column(UUID(as_uuid=True), nullable=False)
     comment_id = Column(UUID(as_uuid=True), nullable=True)
     comment = Column(String, nullable=False)
