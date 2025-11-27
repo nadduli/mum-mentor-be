de15c9ab64aa16cdab7bec6ffd17b510283049b2
idyweb <inyangidongesit22@gmail.com>
2025-11-27 16:45:58 +0100

    Resolve merge conflicts in models/__init__.py

--- api/v1/models/__init__.py
+++ api/v1/models/__init__.py
@@ -1,29 +1,45 @@
-
-from .albums import Album
-from .memories import Memory
-from .photos import Photos
-
-from api.v1.models.milestones import Milestone
-from api.v1.models.categories import Category
-
-__all__ = ["Milestone", "Category"]
-
-from .albums import Album  
-from .memories import Memory  
-from .photos import Photos  
-from .user.user import User  
-
-
 # Import model classes so they are registered on package import
 from .albums import Album  # noqa: F401
 from .memories import Memory  # noqa: F401
 from .photos import Photos  # noqa: F401
 from .user.user import User  # noqa: F401
+
+# Community models
 from .community import Post, PostPhoto, PostLikes, PostComments  # noqa: F401
+
+# Resource models
 from .resource import Resource, ResourceMedia, ResourceLikes, ResourceComment, SavedForLater  # noqa: F401
 
+# Journal models
 from .journal.journal import Journal  # noqa: F401
 from .journal.journal_photos import JournalPhoto  # noqa: F401
 from .journal.journal_category import JournalCategory  # noqa: F401
 from .journal.journal_likes import JournalLike  # noqa: F401
-from .journal.journal_comments import JournalComment  # noqa:
+from .journal.journal_comments import JournalComment  # noqa: F401
+
+# Milestone and Category models
+from .milestones import Milestone  # noqa: F401
+from .categories import Category  # noqa: F401
+
+__all__ = [
+    "Album",
+    "Memory",
+    "Photos",
+    "User",
+    "Post",
+    "PostPhoto",
+    "PostLikes",
+    "PostComments",
+    "Resource",
+    "ResourceMedia",
+    "ResourceLikes",
+    "ResourceComment",
+    "SavedForLater",
+    "Journal",
+    "JournalPhoto",
+    "JournalCategory",
+    "JournalLike",
+    "JournalComment",
+    "Milestone",
+    "Category",
+]
