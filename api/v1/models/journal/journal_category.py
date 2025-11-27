9045a11777ae4909c8312c2d0d33cd0515bd8287
aluyapeter <aluyapeter7@gmail.com>
2025-11-27 14:35:49 +0100

    refactor(journal): update journal models and add schemas

--- api/v1/models/journal/journal_category.py
+++ api/v1/models/journal/journal_category.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import relationship
 from api.db.base_model import BaseModel
 import uuid
 
-class JournalPhotos(BaseModel):
-    __tablename__ = "journal_photos"
+class JournalCategory(BaseModel):
+    __tablename__ = "journal_categories"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    journal_id = Column(UUID(as_uuid=True), ForeignKey("journal.id"), nullable=False)
-    url = Column(String, nullable=False)
-
-    journal = relationship("Journal", back_populates="photos")
+    name = Column(String, nullable=False)
+    
+    journal = relationship("Journal", back_populates="category")
