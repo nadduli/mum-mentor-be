f4383660693a6d0744649759c8d3c105d97019b3
Mr-kings042 <okohkings042@gmail.com>
2025-11-28 09:52:12 +0100

    refactor(community): remove unnecessary blank lines in route and service files
    fix: re-implement community post feature after rever

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -12,6 +12,8 @@ from .admin import router as admin_router
 from .faq import router as faq_router
 from .user_settings import router as user_settings_router
 from .memories import router as memories_router
+from .community_posts import router as  community_posts_router
+
 
 from .task.list_task import router as task_list_router
 from .task.create_task import router as create_task_router
@@ -39,7 +41,7 @@ from .journal import journal_entry_router
 # from .resource import router as resource_router
 from .resource_media import router as resource_media_router
 from .community import router as community_router
-#from .community_posts import router as community_posts_router
+from .community_posts import router as community_posts_router
 from .resource.save_resource_for_later import save_resources_router
 
 app = APIRouter()
@@ -91,7 +93,7 @@ app.include_router(faq_router)
 app.include_router(user_settings_router)
 app.include_router(downloads_router)
 app.include_router(memories_router)
-#app.include_router(community_posts_router)
+app.include_router(community_posts_router)
 app.include_router(community_router)
 #app.include_router(resource_router)
 app.include_router(save_resources_router)
---
+++ api/v1/schemas/community_posts.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field, ConfigDict
+
+
+
+class PostCreateRequest(BaseModel):
+    title: str = Field(...,min_length=1, max_length=200)
+    content: str = Field(...)
+
+
+class PostResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    user_id: UUID
+    title: str
+    content: str
+    views: int
+    created_at: datetime
