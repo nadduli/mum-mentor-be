bbf47f7a0da625a59213abc750178242c2ed3f38
idyweb <inyangidongesit22@gmail.com>
2025-11-28 11:43:10 +0100

    feat(journal): get all journal entries

--- api/v1/routes/journal/__init__.py
+++ api/v1/routes/journal/__init__.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 from .get_entry_by_category import journal_entry_router as get_by_category_router
 from .create_journal_entry import router as create_journal_router
 from .edit_journal_entry import router as edit_journal_router
+from .get_all_entries import router as get_all_entries_router
 
 journal_entry_router = APIRouter()
 journal_entry_router.include_router(create_journal_router)
 journal_entry_router.include_router(get_by_category_router)
+journal_entry_router.include_router(get_all_entries_router)
 
 router = APIRouter()
 router.include_router(create_journal_router)
