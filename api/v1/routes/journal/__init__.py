04374a2d5050e291847df4f37054491c2821adc6
Joseph Ezekiel <theolujay@gmail.com>
2025-11-30 06:24:15 +0100

    refactor: consolidate journal routes

--- api/v1/routes/journal/__init__.py
+++ api/v1/routes/journal/__init__.py
@@ -1,20 +1,11 @@
+"""
+Journal module initialization.
+"""
+
 from fastapi import APIRouter
-from .get_entry_by_category import journal_entry_router as get_by_category_router
-from .create_journal_entry import router as create_journal_router
-from .edit_journal_entry import router as edit_journal_router
-from .get_all_entries import router as get_all_entries_router
-from .delete_journal_entry import router as delete_journal_router
+from .routes import router as journal_routes
 
-journal_entry_router = APIRouter()
-journal_entry_router.include_router(create_journal_router)
-journal_entry_router.include_router(get_by_category_router)
-journal_entry_router.include_router(get_all_entries_router)
-journal_entry_router.include_router(delete_journal_router)
+journal_router = APIRouter()
+journal_router.include_router(journal_routes)
 
-router = APIRouter()
-router.include_router(create_journal_router)
-router.include_router(get_by_category_router)
-router.include_router(edit_journal_router)
-router.include_router(delete_journal_router)
-
-__all__ = ["journal_entry_router", "router"]
+__all__ = ["journal_router"]
