d030d4e76609601407bdafec8c1de3f058140ff9
Ibukun Babatunde <96484818+babatundeibukun@users.noreply.github.com>
2025-11-30 10:20:16 +0100

    Import SQLAlchemyError for error handling

--- api/v1/services/community_posts.py
+++ api/v1/services/community_posts.py
@@ -2,6 +2,7 @@ from typing import Optional, Tuple
 from uuid import UUID
 
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 from api.utils.logger import logger
 from api.v1.models.community.posts import Post
